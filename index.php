13c1721a9391f12d7933467d4e38d86850c83199
Louka <louka.le.guyader@gmail.com>
2022-06-01 09:17:34 +0200

    ajout style index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+ 
+  <head>
+    <meta charset="utf-8">
+    <title>Lorem Ipsum</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+ 
+  <body>
+  </body>
+ 
+</html>
