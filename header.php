1eae2d9cde7616781f1c656e8ee07a816bb08b10
LaPoloche <buchon.paul@gmail.com>
2022-06-15 08:21:11 +0200

    hedaer

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="mystyle.css"> 
+    <link rel="stylesheet" href="style.css"> 
     <title>Confiz</title>
 </head>
 <body>
