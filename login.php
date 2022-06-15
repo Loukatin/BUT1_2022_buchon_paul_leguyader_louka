afe4044f2552d5e811678ffab0080ff7ebcbfe19
Louka <louka.le.guyader@gmail.com>
2022-06-15 11:05:00 +0200

    login

--- login.php
+++ login.php
@@ -1,14 +1,10 @@
 <?php require_once("header.php")?>
-
-<div class="conteneur"> 
-   
-    <div class="carre"></div>
-    <div class="username"></div>
-    <div class="password"></div>
-    <button class="connexion" type="button"><a href=""> Connexion</a></button>
-
-
-</div>
-      
-</body>
+    <div class="conteneur"> 
+        <div class="carre">
+                <input type="text" id="username" name="Utilisateur" placeholder="Utilisateur" required>
+                <input type="text" id="password" name="Mot de passe" placeholder="Mot de passe" required>
+                <button class="connexion" type="button"><a href=""> Connexion</a></button>
+        </div>
+    </div>   
+    </body>
 </html>
