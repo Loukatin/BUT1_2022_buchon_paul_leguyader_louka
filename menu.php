d19cb521e44bae79da09f5985c46d6baee01ce41
LaPoloche <buchon.paul@gmail.com>
2022-06-15 09:02:06 +0200

    style

---
+++ menu.php
@@ -0,0 +1,27 @@
+<?php require_once("header.php")?>
+        <div class="top-nav">
+            <p class="nom-prenom">Paul BUCHON</p>
+        </div>
+        <h2>Choisissez votre boutique</h2>
+        <div class="menu-filter"></div>
+        <div class="card-list">
+            <div class="card">
+                <div class="card-illustration">
+                    <img>
+                </div>
+                <div class="nom-boutique">
+                    <p>Nom boutique:</p>
+                    <div class="nom-content">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="adresse-boutique">
+                    <p>Adresse:</p>
+                    <div class="adresse-content">
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
