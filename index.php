070ffca5ed872c7b60ccedb6ad62f481b33d4893
LaPoloche <buchon.paul@gmail.com>
2022-06-22 09:23:07 +0200

    REgister init

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
- 
-  <head>
-    <meta charset="utf-8">
-    <title>Lorem Ipsum</title>
-    <link rel="stylesheet" href="styles.css">
-  </head>
- 
-  <body>
-      <h1>jibvghcj</h1>
-  </body>
- 
-</html>
+<?php
+    header('location:login.php');
+?>
