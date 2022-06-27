32a9728ef236ee7c7dba6045d44a2817c542c018
LaPoloche <buchon.paul@gmail.com>
2022-06-27 17:35:29 +0200

    commentaires

--- admin.php
+++ admin.php
@@ -1,9 +1,12 @@
 <?php 
 require_once("menu.php");
 require_once("functions.php");
+
+//On empêches les client d'accéder à cette page
 if($_SESSION['session']['type']=='client'){
     header('location:boutiquec.php');
 }
+//Ceci est exécuté lorsqu'on clique sur Promouvoir ou rétrograder prom_user() change les gérant en client et vice-versa
 if(isset($_GET['id'])){
     if(get_user_by_id($_GET['id'])[0]['type'] == 'client'){
         prom_user($_GET['id'], 'gerant');
@@ -15,6 +18,7 @@ if(isset($_GET['id'])){
     }
 }
 
+//Par mesure de sécurité et empêcher qu'il n'y ai plus d'admin, un admin ne peut pas s'enlever son rôle car il n'aparait pas dans sa page d'administration
 $users = list_all_user();
 foreach($users as $index => $user) {
     if($user['id']==$_SESSION['session']['id']){
@@ -26,6 +30,7 @@ foreach($users as $index => $user) {
         <div class="boutique-container">
             <div class="card-list">
                 <?php
+                    //Affihe des cartes différentes en fonction de si c'est un client ou bien à gérant. Le style change en conséquence
                     foreach($users as $index => $user) {
                         if($user['type']=='client'){
                             echo("<div id='ccard".$user['id']."' class='card user-card'>");
